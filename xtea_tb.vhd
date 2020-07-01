c049080103b9cf66a69db8a87c49a883b67b9379
Your Name <you@example.com>
2020-07-01 15:38:57 +0200

    fertig

--- xtea_tb.vhd
+++ xtea_tb.vhd
@@ -32,22 +32,29 @@ begin
 	comp_encipher : xtea
 		port map(clk => clk, rst => rst, en => en, rdy => enc_rdy, op => '0', v0_in => v0_in, v1_in => v1_in, key0=>key0, key1=>key1, key2=>key2, key3=>key3, sum_in=>sum_in, delta =>delta, v0_out => v0_enc_out, v1_out => v1_enc_out);
 	comp_decipher : xtea
-		port map(clk => clk, rst => rst, en => enc_rdy, rdy => rdy, op => '1', v0_in => v0_enc_out, v1_in => v1_enc_out, key0 => key0, key1 => key1, key2 => key2, key3 => key3, delta => delta,  
+		port map(clk => clk, rst => rst, en => enc_rdy, rdy => rdy, op => '1', v0_in => v0_enc_out, v1_in => v1_enc_out, key0 => key0, key1 => key1, key2 => key2, key3 => key3, sum_in => x"8dde6e40", delta => delta, v0_out => v0_out, v1_out => v1_out);  
 	
+	
+			clk_proc : process
+			begin
+				clk <= '1';
+				wait for 10 ns;
+				clk <= '0';
+				wait for 10 ns;
+			end process;
 
-			 clk_proc : process
+			 stim_proc : process
 			 begin
 				 rst <='1';
 				 wait for 100 ns;
 				 en <= '1';
-				 op <= '1';
 				 rst   <= '0';
-				 v0_in <= x"00000000";
-				 v1_in <= x"00000000";
-				 key0  <= x"00000000";
-				 key1  <= x"00000000";
-				 key2  <= x"00000000";
-				 key3  <= x"00000000";
+				 v0_in <= x"00000020";
+				 v1_in <= x"00000020";
+				 key0  <= x"00000020";
+				 key1  <= x"00000020";
+				 key2  <= x"00000020";
+				 key3  <= x"00000020";
 				 delta <= x"9E3779B9";
 				 sum_in<= x"00000000";
 			wait for 50000 ns;
